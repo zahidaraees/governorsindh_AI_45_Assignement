8169cbcbb5c671210420f236b85639b93218c143
zahidaraees <160402259+zahidaraees@users.noreply.github.com>
2024-03-17 06:19:57 +0500

    Add files via upload

---
+++ Q_2/main.js
@@ -0,0 +1,4 @@
+"use strict";
+//Q2- 2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+let PersonName = "Zahida Raees";
+console.log("Hello", PersonName + ',', "would you like to learn some Python today?");
---
+++ Q_2/main.ts
@@ -0,0 +1,3 @@
+//Q2- 2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+let PersonName:String = "Zahida Raees";
+console.log("Hello", PersonName+',',"would you like to learn some Python today?");
